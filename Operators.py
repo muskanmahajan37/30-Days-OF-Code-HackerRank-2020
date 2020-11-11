0b5a34f066efec1a4c53c592f1c3b82ff3926a49
MUSKAN MAHAJAN <45875834+muskanmahajan37@users.noreply.github.com>
2020-11-11 06:33:33 +0530

    Day-2 :- OPERATORS

---
+++ Operators.py
@@ -0,0 +1,19 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(meal_cost, tip_percent, tax_percent):
+
+if __name__ == '__main__':
+    meal_cost = float(raw_input())
+
+    tip_percent = int(raw_input())
+
+    tax_percent = int(raw_input())
+
+    solve(meal_cost, tip_percent, tax_percent)
